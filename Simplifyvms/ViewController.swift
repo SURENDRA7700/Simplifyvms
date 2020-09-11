702d9a0bf67b9b0b6c30c8fd1419207f018a3163
Surendra <ksurendrakumar01@gmail.com>
2020-09-11 20:12:35 +0530

    Initial Commit

---
+++ Simplifyvms/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Simplifyvms
+//
+//  Created by surendra kumar k on 11/09/20.
+//  Copyright © 2020 Semanoor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
